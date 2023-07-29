2e3ccaf231031501674f4e51fa5c995512a6e3e4
devunoorivignan24 <devunoorivignan24@gmail.com>
2023-07-29 20:56:27 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw=new RWOnly();
+        rw.setName("vignan");
+        System.out.println(rw.getName());
+    //java: name has private access in com.driver.RWOnly
+        // java: name has private access in com.driver.RWOnly
+    }
 }
